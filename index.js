44b0430b5ddf8717eccd5168865707918ddaa40f
Ahasan-16 <mdahasan1916@gmail.com>
2025-09-20 12:56:44 +0600

    socket.io configure client and server side

---
+++ index.js
@@ -0,0 +1,32 @@
+const express=require('express');
+const app=express();
+const http=require('http');
+const expressServer=http.createServer(app);
+
+
+//socket je bhabe configure korbo
+const {Server}=require('socket.io');
+//Ei kane Server ekta class er object toiri korbo,bitore expressServer ta pass korabo
+const io=new Server(expressServer);
+
+//socket tik tak kaj korche ki na tar jonno connection test korte pari
+
+io.on('connection', (socket)=>{
+    console.log('connection connected');
+    socket.on('disconnect',()=>{
+        console.log('connection disconnected');
+    })
+})
+
+
+
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html');
+})
+
+
+
+expressServer.listen(3000,function(){
+    console.log("Server started on port 3000");
+});
